57457129d4577129da716ffff12e545a275572ca
ArunKumar_D <arunkumard1018@gmail.com>
2025-02-16 22:57:38 +0530

    Fixing Server Connect Issue

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import http from 'http';
 import logger from './lib/loggerConfig';
 import { logReqRes } from './middlewares/logger';
-import userRouter from './routes/user';
 import { gadgetsRouter } from './routes/gadgets';
-new PrismaClient();
+import userRouter from './routes/user';
 
 const app = express();
 const server = http.createServer(app);
@@ -20,7 +18,7 @@ app.get("/", (req, res) => {
     res.send("Hello Agent");
 });
 
-logger.info("Connecting To Mongo DB Server....");
+logger.info("Connecting To Server....");
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`Development server running on http://localhost:${PORT}`);
